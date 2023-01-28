7261669ffd8507f6dd0df9a819b5e2ed16c88024
Andreas Egge Lye <Andreasel@uia.no>
2023-01-28 13:31:49 +0100

    Ferdig med oblig 1b

--- oblib1b/Min1steRfil.R
+++ oblib1b/Min1steRfil.R
@@ -1,20 +1,36 @@
-
-
 lengder = read.csv("terning.csv", sep=",")
 lengder
 data <- data.frame(lengder$lengde,lengder$hoyde)
-
-plot(data$lengder.lengde, data$lengder.hoyde)
-model <- lm(data$lengder.hoyde ~ data$lengder.lengde)
+minX = data$lengder.lengde
+minY = data$lengder.hoyde
+#Plotte alle punktenne
+plot(minX, minY, ylim = c(5,30), ylab = "Høyde, cm", xlab = "Lengde, cm")
+model <- lm(minY ~ minX)
+#Plotte regresjonslinjen 
 abline(model, col="red")
-model
+min_summary = summary(model)
+model # Printer ut alpha og beta
+
+alfa = min_summary$coefficients[1]
+beta = min_summary$coefficients[2]
+minPredY = alfa + minX * beta 
+
+#Plotte orange linjesegmenter mellom punktene og regresjonslinja
+segments(minX,minY,minX,minPredY,col="orange")
+SSe = (length(data$lengder.lengde)-2)*min_summary$sigma^2
+n = length(data$lengder.lengde)
+SSe
+s_e = sqrt((1/(n-2))*SSe)
+
+var(data.frame(minX, minY))
+# --------------------------------------------------------------------------------------------------------
+# Her ville vi bare teste om vi kunne rekne med matrisene i R Studio. 
 #Lager en matrise med alpha og beta = X
-X = cbind(data$lengder.lengde^0, data$lengder.lengde)
-#Matrise X
+X = cbind(minX^0, minX)
+#Printe ut matrise X
 X
 # Transponert Matrise X
 t(X)
-
 # Matrise X^t * X. 2x2 matrise
 crossprod(X)
 # Eller:
@@ -24,8 +40,8 @@ t(X)%*% X
 solve(t(X) %*% X)
 
 #For å finne X^t * y(vektor):
-t(X) %*% data$lengder.hoyde
+t(X) %*% minY
 
-beta = solve(t(X) %*% X) %*% t(X) %*% data$lengder.hoyde
+beta = solve(t(X) %*% X) %*% t(X) %*% minY
 beta
-t(X) %*% data$lengder.hoyde
+t(X) %*% minY
